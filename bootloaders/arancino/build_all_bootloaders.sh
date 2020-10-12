2a12a8b13b16ed45203ebf6ab122cf808a45366e
Arturo Rinaldi <arty.net2@gmail.com>
2020-10-12 19:17:09 +0200

    now all bootloaders are built

--- bootloaders/arancino/build_all_bootloaders.sh
+++ bootloaders/arancino/build_all_bootloaders.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 
-BOARD_ID=arancino NAME=samd21_sam_ba make clean all
+BOARD_ID=arancino NAME=samd21_sam_ba_arancino make clean all
+
+BOARD_ID=arancino_mignon NAME=samd21_sam_ba_arancino_mignon make clean all
+
+BOARD_ID=arancino_mignon NAME=samd21_sam_ba_arancino_mignon_RS485 RS485=RS485_ENABLED make clean all
 
 echo Done building bootloaders!
